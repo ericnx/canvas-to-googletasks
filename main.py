09dbed9dddf5ddaea49730928014ce96ccff8b61
Eric Nguyen <eriicnguyen1@gmail.com>
2025-01-09 13:05:07 -0800

    got assignments from the courses

--- main.py
+++ main.py
@@ -7,22 +7,40 @@ CANVAS_TOKEN = os.getenv("CANVAS_TOKEN")
 CANVAS_URL = url = os.getenv("CANVAS_URL")
 
 h = {"Authorization": f"Bearer {CANVAS_TOKEN}"}
-p = {"include": ["term"]}
+p = {"per_page": 1000}
 
-course_ids_and_names = []
-while url:
+# gets the courses using the student's Canvas token
+course_list = {}
+while url: # O(P * C). P = # of pages, C = # of courses
     r = requests.get(url, headers=h, params=p)
-    courses = r.json()
+    if r.status_code == 200:
+        courses = r.json()
 
-    for course in courses:
-        course_id = course.get("id")
-        course_name = course.get("name")
-        if course_id and course_name:
-            course_ids_and_names.append({"id": course_id, "name": course_name})
-            
-    url = None
-    for link in r.headers.get("Link","").split(","):
-        if 'rel="next"' in link:
-            url = link.split(";")[0].strip("<>")
+        for course in courses:
+            if course.get("id") and course.get("name"):
+                course_list[course.get("id")] = course.get("name")
 
-print(course_ids_and_names)
+        url = None
+        for link in r.headers.get("link").split(","): # breaks into separate links
+            if 'rel="next"' in link: # rel = relation, looks for the "next" page/relation
+                url = link.split(";")[0].strip("<>") # gets the clean url of the "next" page which is the first link
+
+        print(f"{course_list}\n")
+    else:
+        print(f"Failed to get the courses for URL:{url} - Status:{r.status_code}")
+
+# gets the assignments using the course ids and names
+assignment_list = {}
+for course_id in course_list.keys():
+    url = f"{CANVAS_URL}/{course_id}/assignments"
+    r = requests.get(url, headers=h, params=p)
+    
+    if r.status_code == 200:
+        assignments = r.json()
+        print(f"Assignments for course: {course_list[course_id]}")
+        for assignment in assignments:
+            if assignment.get("id") and assignment.get("name") and assignment.get("due_at"):
+                assignment_list[assignment.get("id")] = f"{assignment.get("name")} due at {assignment.get("due_at", "No due")}"
+                print(f"- {assignment_list[assignment.get("id")]}")
+    else:
+        print(f"Failed to get the assignments for ID:{course_id} - Status:{r.status_code}")
