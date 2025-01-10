09f58e271d457d1be87a3f7cfc11c15cb8b66f3e
Eric Nguyen <eriicnguyen1@gmail.com>
2025-01-10 13:03:42 -0800

    main()

--- main.py
+++ main.py
@@ -3,54 +3,58 @@ import os
 from dotenv import load_dotenv
 from datetime import date
 
-load_dotenv()
-CANVAS_TOKEN = os.getenv("CANVAS_TOKEN")
-CANVAS_URL = url = os.getenv("CANVAS_URL")
+def main():
+    load_dotenv()
+    CANVAS_TOKEN = os.getenv("CANVAS_TOKEN")
+    CANVAS_URL = url = os.getenv("CANVAS_URL")
 
-h = {"Authorization": f"Bearer {CANVAS_TOKEN}"}
-p = {"per_page": 1000}
+    h = {"Authorization": f"Bearer {CANVAS_TOKEN}"}
+    p = {"per_page": 1000}
 
-# gets the courses using the student's Canvas token
-course_list = {}
-while url: # O(P * C). P = # of pages, C = # of courses
-    r = requests.get(url, headers=h, params=p)
-    if r.status_code == 200:
-        courses = r.json()
+    # gets the courses using the student's Canvas token
+    course_list = {}
+    while url: # O(P * C). P = # of pages, C = # of courses
+        r = requests.get(url, headers=h, params=p)
+        if r.status_code == 200:
+            courses = r.json()
 
-        for course in courses:
-            if course.get("id") and course.get("name"):
-                course_list[course.get("id")] = course.get("name")
+            for course in courses:
+                if course.get("id") and course.get("name"):
+                    course_list[course.get("id")] = course.get("name")
 
-        url = None
-        for link in r.headers.get("link").split(","): # breaks into separate links
-            if 'rel="next"' in link: # rel = relation, looks for the "next" page/relation
-                url = link.split(";")[0].strip("<>") # gets the clean url of the "next" page which is the first link
-                
-        print(f"{course_list}\n")
-    else:
-        print(f"Failed to get the courses for URL:{url} - Status:{r.status_code}")
+            url = None
+            for link in r.headers.get("link").split(","): # breaks into separate links
+                if 'rel="next"' in link: # rel = relation, looks for the "next" page/relation
+                    url = link.split(";")[0].strip("<>") # gets the clean url of the "next" page which is the first link
 
-# gets the assignments' ids, names, and due dates
-assignment_list = {}
-today = date.today()
+            print(f"{course_list}\n")
+        else:
+            print(f"Failed to get the courses for URL:{url} - Status:{r.status_code}")
 
-for course_id in course_list.keys(): # O(C * A). C = # of courses, A = # of assignments
-    url = f"{CANVAS_URL}/{course_id}/assignments" # creates a new url to get the course's assignments
-    r = requests.get(url, headers=h, params=p)
-    
-    if r.status_code == 200:
-        assignments = r.json()
-        print(f"Assignments for course: {course_list[course_id]}")
+    # gets the assignments' ids, names, and due dates
+    assignment_list = {}
+    today = date.today()
 
-        latest_assignment = assignments[-1].get("due_at")
-        if latest_assignment is None or date.fromisoformat(latest_assignment[:10]) < today:
-            print("- No assignments")
-            continue
+    for course_id in course_list.keys(): # O(C * A). C = # of courses, A = # of assignments
+        url = f"{CANVAS_URL}/{course_id}/assignments" # creates a new url to get the course's assignments
+        r = requests.get(url, headers=h, params=p)
+        
+        if r.status_code == 200:
+            assignments = r.json()
+            print(f"Assignments for course: {course_list[course_id]}")
 
-        for assignment in assignments:
-            if assignment.get("id") and assignment.get("name") and assignment.get("due_at"):
-                if date.fromisoformat(assignment.get("due_at")[:10]) >= today: # only get today's or future assignments
-                    assignment_list[assignment.get("id")] = f"{assignment.get("name")} due at {assignment.get("due_at", "No due date")}"
-                    print(f"- {assignment_list[assignment.get("id")]}")   
-    else:
-        print(f"Failed to get the assignments for ID:{course_id} - Status:{r.status_code}")
+            latest_assignment = assignments[-1].get("due_at")
+            if latest_assignment is None or date.fromisoformat(latest_assignment[:10]) < today:
+                print("- No assignments")
+                continue
+
+            for assignment in assignments:
+                if assignment.get("id") and assignment.get("name") and assignment.get("due_at"):
+                    if date.fromisoformat(assignment.get("due_at")[:10]) >= today: # only get today's or future assignments
+                        assignment_list[assignment.get("id")] = f"{assignment.get("name")} due at {assignment.get("due_at", "No due date")}"
+                        print(f"- {assignment_list[assignment.get("id")]}")   
+        else:
+            print(f"Failed to get the assignments for ID:{course_id} - Status:{r.status_code}")
+
+if __name__ == "__main__":
+    main()
